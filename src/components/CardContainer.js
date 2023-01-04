d331dd2acfc804c1b9a9c6134c28a07685fc0c00
JoaoMateus98 <joaobsu98@gmail.com>
2023-01-04 17:30:09 -0500

    used Fisher Yates shuffle to shuffle cards

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,18 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import "../styles/CardContainer.css";
 
 const CardContainer = (props) => {
-  const [cardList, setCardList] = useState([
-    { key: "python" },
-    { key: "javascript" },
-    { key: "basic" },
-  ]);
+  const [cardList, setCardList] = useState({
+    array: [{ name: "python" }, { name: "javascript" }, { name: "basic" }],
+  });
+
+  // Fisher–Yates shuffle from stack overflow
+  const shuffleCards = () => {
+    const array = [...cardList.array];
+
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    setCardList({ array: array });
+  };
+
+  // shuffle cards in the beginnig
+  useEffect(() => {
+    shuffleCards();
+  }, []);
 
   return (
     <div className="card-container">
-      {cardList.map((card) => {
-        return <Card name={card.key} />;
+      {cardList.array.map((card) => {
+        return <Card key={card.name} name={card.name} />;
       })}
     </div>
   );
