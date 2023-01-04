1cf8da272ed842049a579fa737011a6e0219c0a4
JoaoMateus98 <joaobsu98@gmail.com>
2023-01-04 16:17:12 -0500

    scoreBoard now displays current and high score

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ScoreBoard from "./components/ScoreBoard";
 import CardContainer from "./components/CardContainer";
 import "./styles/App.css";
 
 const App = () => {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
   return (
     <div className="main-app-container">
-      <ScoreBoard />
+      <ScoreBoard currentScore={currentScore} highScore={highScore} />
       <CardContainer />
     </div>
   );
